5ce53193563399fa971d95c790fa4a8e26292968
Sam Pierson <sam@pivotallabs.com>
2010-02-08 14:18:51 -0800

    Added tests for SeleniumConfig[:selenium_browser_key]

--- spec/selenium_config_spec.rb
+++ spec/selenium_config_spec.rb
@@ -23,16 +23,37 @@ describe "SeleniumConfig" do
       @selenium_config = SaucelabsAdapter::SeleniumConfig.new('stanza_saucelabs_firefox_linux', SELENIUM_YML_FIXTURE_FILE)
     end
 
+    describe "selenium_browser_key" do
+      before do
+        @browser_data = JSON.parse(@selenium_config[:selenium_browser_key])
+      end
+
+      {
+        'username'        => "YOUR-SAUCELABS-USERNAME",
+        'access-key'      => "YOUR-SAUCELABS-ACCESS-KEY",
+        'os'              => "Linux",
+        'browser'         => "firefox",
+        'browser-version' => "3."
+      }.each do |browser_data_key, browser_data_value|
+        it "should contain a #{browser_data_key} of #{browser_data_value}" do
+          @browser_data[browser_data_key].should == browser_data_value
+        end
+      end
+
+      it "should contain a job_name of our hostname" do
+        @browser_data['job-name'].should == Socket.gethostname
+      end
+    end
+
     describe "#configure_polonium" do
       before do
         @polonium_configuration = mock("Polonium::Configuration")
-        @json_creds = '{"username": "YOUR-SAUCELABS-USERNAME", "access-key": "YOUR-SAUCELABS-ACCESS-KEY", "os": "Linux", "browser": "firefox", "browser-version": "3."}'
       end
 
       it "should call the appropriate configuration methods on the polonium configuration object" do
         @polonium_configuration.should_receive(:'selenium_server_host=').with("saucelabs.com")
         @polonium_configuration.should_receive(:'selenium_server_port=').with("4444")
-        @polonium_configuration.should_receive(:'browser=')
+        @polonium_configuration.should_receive(:'browser=').with(@selenium_config[:selenium_browser_key])
         @polonium_configuration.should_receive(:'external_app_server_host=').with(@selenium_config[:application_address])
         @polonium_configuration.should_receive(:'external_app_server_port=').with("80")
 
@@ -48,7 +69,7 @@ describe "SeleniumConfig" do
       it "should call the appropriate configuration methods on the webrat configuration object" do
         @webrat_configuration.should_receive(:'selenium_server_address=').with("saucelabs.com")
         @webrat_configuration.should_receive(:'selenium_server_port=').with("4444")
-        @webrat_configuration.should_receive(:'selenium_browser_key=')
+        @webrat_configuration.should_receive(:'selenium_browser_key=').with(@selenium_config[:selenium_browser_key])
         @webrat_configuration.should_receive(:'application_address=').with(@selenium_config[:application_address])
         @webrat_configuration.should_receive(:'application_port=').with("80")
 
