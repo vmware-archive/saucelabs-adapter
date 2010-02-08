e69a61d0fdac96d67f2c8a5859311098479c3a4d
Sam Pierson <sam@pivotallabs.com>
2010-02-08 14:47:19 -0800

    Test a local stanza with SeleniumConfig too.

--- spec/selenium_config_spec.rb
+++ spec/selenium_config_spec.rb
@@ -18,9 +18,65 @@ end
 
 describe "SeleniumConfig" do
 
+  context "given a local configuration" do
+    before do
+      @selenium_config = SaucelabsAdapter::SeleniumConfig.new('local', SELENIUM_YML_FIXTURE_FILE)
+    end
+
+    describe "#start_tunnel?" do
+      it "should return false" do
+        @selenium_config.start_tunnel?.should be_false
+      end
+    end
+
+    describe "selenium_browser_key" do
+      it "should contain just the string from the yml file" do
+        @selenium_config['selenium_browser_key'].should == "*chrome /Applications/Firefox.app/Contents/MacOS/firefox-bin"
+      end
+    end
+
+    describe "#configure_polonium" do
+      before do
+        @polonium_configuration = mock("Polonium::Configuration")
+      end
+
+      it "should call the appropriate configuration methods on the polonium configuration object" do
+        @polonium_configuration.should_receive(:'selenium_server_host=').with("127.0.0.1")
+        @polonium_configuration.should_receive(:'selenium_server_port=').with("4444")
+        @polonium_configuration.should_receive(:'browser=').with(@selenium_config[:selenium_browser_key])
+        @polonium_configuration.should_receive(:'external_app_server_host=').with("127.0.0.1")
+        @polonium_configuration.should_receive(:'external_app_server_port=').with("4000")
+
+        @selenium_config.configure_polonium(@polonium_configuration)
+      end
+    end
+
+    describe "#configure_webrat" do
+      before do
+        @webrat_configuration = mock("Webrat::Configuration")
+      end
+
+      it "should call the appropriate configuration methods on the webrat configuration object" do
+        @webrat_configuration.should_receive(:'selenium_server_address=').with("127.0.0.1")
+        @webrat_configuration.should_receive(:'selenium_server_port=').with("4444")
+        @webrat_configuration.should_receive(:'selenium_browser_key=').with(@selenium_config[:selenium_browser_key])
+        @webrat_configuration.should_receive(:'application_address=').with("127.0.0.1")
+        @webrat_configuration.should_receive(:'application_port=').with("4000")
+
+        @selenium_config.configure_webrat(@webrat_configuration)
+      end
+    end
+  end
+
   context "given a saucelabs/firefix/linux configuration" do
     before do
       @selenium_config = SaucelabsAdapter::SeleniumConfig.new('stanza_saucelabs_firefox_linux', SELENIUM_YML_FIXTURE_FILE)
+    end
+
+    describe "#start_tunnel?" do
+      it "should return true" do
+        @selenium_config.start_tunnel?.should be_true
+      end
     end
 
     describe "selenium_browser_key" do
