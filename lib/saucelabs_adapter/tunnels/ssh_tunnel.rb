ecf9fc7ea2814d56ec5f4e88c7e08c71204e3eb0
Sam Pierson & Sarah Mei <slm+sam+sarah@pivotallabs.com>
2010-04-13 15:32:19 -0700

    SshTunnel#shutdown now actually does something again, but with a 15 second timeout.

--- lib/saucelabs_adapter/tunnels/ssh_tunnel.rb
+++ lib/saucelabs_adapter/tunnels/ssh_tunnel.rb
@@ -10,18 +10,25 @@ module SaucelabsAdapter
       say "Setting up SSH reverse tunnel from #{@se_config.application_address}:#{@se_config.application_port} to localhost:#{@se_config.tunnel_to_localhost_port}"
       options = @se_config.tunnel_password ? { :password => @se_config.tunnel_password } : { :keys => @se_config.tunnel_keyfile }
       @gateway = Net::SSH::Gateway.new(@se_config.application_address, @se_config.tunnel_username, options)
-      @port = @gateway.open_remote(@se_config.tunnel_to_localhost_port.to_i, "127.0.0.1", @se_config.application_port.to_i, "0.0.0.0")
+      @host_and_port = @gateway.open_remote(@se_config.tunnel_to_localhost_port.to_i, "127.0.0.1", @se_config.application_port.to_i, "0.0.0.0")
     end
 
     def shutdown
-      return
-      # just return; tunnel is all in transient memory, and we don't want to hang or abort the whole test suite anyway
-      # This could potentially be a problem when opening an SshTunnel multiple times during a single interpreter, but we'll see...
-      # if @gateway
-      #   say "Shutting down ssh reverse tunnel"
-      #   @gateway.close(@port) if @port
-      #   @gateway.shutdown! if @gateway
-      # end
+      # We have experienced problems with the tunnel shutdown hanging.
+      # Previously the method was a no-op and we just exited the process which had the effect of closing the tunnel.
+      # However we are now running multiple tunnels in one process (sequentially), so we need to actually do a shutdown.
+      # So let's add a timeout.
+      if @gateway
+        begin
+          Timeout::timeout(15) do
+            say "Shutting down ssh reverse tunnel"
+            @gateway.close_remote(*@host_and_port) if @host_and_port
+            @gateway.shutdown! if @gateway
+          end
+        rescue Timeout::Error
+          say "tunnel shutdown timed out"
+        end
+      end
     end
   end
 end
