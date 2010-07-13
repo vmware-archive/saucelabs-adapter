4f7c035a22c47127b32f03768a9d95c1c84878b1
Alexander J. Murmann & Austin Putman <pair+alexander+austin@pivotallabs.com>
2010-07-12 17:41:37 -0700

    manage tunnels and webrat configuration transparently when using Rspec

---
+++ lib/saucelabs_adapter/rspec_adapter.rb
@@ -0,0 +1,28 @@
+if defined?(Spec::Runner)
+
+  module Spec
+    module Runner
+      class Configuration
+        attr_accessor :saucelabs_tunnel
+      end
+    end
+  end
+  Spec::Runner.configure do |config|
+    config.before :all do
+      @selenium_config = SaucelabsAdapter::SeleniumConfig.new(ENV['SELENIUM_ENV'])
+      if @selenium_config.start_tunnel? and config.saucelabs_tunnel.nil?
+        config.saucelabs_tunnel = SaucelabsAdapter::Tunnel.factory(@selenium_config)
+        config.saucelabs_tunnel.start_tunnel
+      end
+    end
+
+    config.before do
+      webrat_config = Webrat.configuration
+      @selenium_config.configure_webrat(webrat_config)
+    end
+
+    at_exit do
+      config.saucelabs_tunnel.shutdown if config.saucelabs_tunnel
+    end
+  end
+end
