fc22136d4dfc63c9ea0fc3aa7ed7fb0d39ec203c
joeyrobert <joey@joeyrobert.org>
2010-09-30 00:48:02 +0800

    do not include all test frameworks by default

--- lib/saucelabs_adapter.rb
+++ lib/saucelabs_adapter.rb
@@ -4,8 +4,5 @@ require 'saucelabs_adapter/tunnels/sauce_tunnel'
 require 'saucelabs_adapter/tunnels/ssh_tunnel'
 require 'saucelabs_adapter/tunnels/other_tunnel'
 require 'saucelabs_adapter/selenium_config'
-require 'saucelabs_adapter/test_unit_adapter'
-require 'saucelabs_adapter/rspec_adapter'
-require 'saucelabs_adapter/jsunit_selenium_support'
 module SaucelabsAdapter
 end
