a99e2db475d6e2850cc50ca4761c3800dbf2e2f1
David Goudreau & Derrick Ko <pair+david+derrick@pivotallabs.com>
2010-11-08 11:14:21 -0800

    fixed sample test
    removed testunit and jsunit related files

--- lib/generators/saucelabs_adapter/templates/sample_selenium_spec.rb
+++ lib/generators/saucelabs_adapter/templates/sample_selenium_spec.rb
@@ -1,8 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/selenium_spec_helper')
 
-describe 'Google Search' do
-  it 'can find Google' do
-    visit 'http://www.google.com'
-    webrat_session.selenium.get_html_source.should contain 'Google'
+describe 'access a webpage' do
+  it 'can find Pivotal Labs' do
+    visit 'http://www.pivotallabs.com'
+    webrat_session.selenium.get_html_source.should contain 'Pivotal Labs'
   end
 end
